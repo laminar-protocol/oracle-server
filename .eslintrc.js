856a46a77260d5afb6594e537adf91df4db1c1b2
Shaopeng Wang <spxwang@gmail.com>
2019-10-24 15:36:39 +1300

    Set up project.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: [
+      './tsconfig.json',
+      './tsconfig.eslint.json'
+    ]
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  rules: {
+  },
+};

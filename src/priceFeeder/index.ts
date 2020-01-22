e2d276c9359879c6eb77f4c94d3b5f44931e97f8
Shaopeng Wang <spxwang@gmail.com>
2020-01-23 11:49:06 +1300

    Laminar oracle server (#22)
    * Laminar feeder.
    
    * Mock LAMI price.
    
    * Remove LAMI price from listing.

--- src/priceFeeder/index.ts
+++ src/priceFeeder/index.ts
@@ -4,6 +4,8 @@ import { newEthFeeder } from './eth/feeder';
 import ethListings from './eth/listings.json';
 import { newAcalaFeeder } from './acala/feeder';
 import acalaListings from './acala/listings.json';
+import { newLaminarFeeder } from './laminar/feeder';
+import laminarListings from './laminar/listings.json';
 
 const startWithFeeder = async (feeder: FeederKind, listings: Listing[]) => {
   await feeder.setup();
@@ -18,6 +20,10 @@ const startFeedingPrice = async () => {
 
   if (process.env.FEED_ACALA === 'true') {
     await startWithFeeder(newAcalaFeeder(), acalaListings);
+  }
+
+  if (process.env.FEED_LAMINAR === 'true') {
+    await startWithFeeder(newLaminarFeeder(), laminarListings);
   }
 };
 
